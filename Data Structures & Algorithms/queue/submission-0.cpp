29a716d8dba8b0074496b28e673876a33430f8f7
Giovanni B. <giocuffietta@gmail.com>
2026-09-24 00:44:39 +0200

    Add: queue - submission-0

---
+++ Data Structures & Algorithms/queue/submission-0.cpp
@@ -0,0 +1,34 @@
+class Deque {
+public:
+
+    std::list<int> q;
+    Deque() {}
+
+    bool isEmpty() {
+        return q.empty();
+    }
+
+    void append(int value) {
+        q.push_back(value);
+    }
+
+    void appendleft(int value) {
+        q.emplace_front(value);
+    }
+
+    int pop() {
+        if(isEmpty()) return -1;
+        int temp = q.back();
+        q.pop_back();
+
+        return temp;
+
+    }
+
+    int popleft() {
+        if(isEmpty()) return -1;
+        int temp = q.front();
+        q.pop_front();
+        return temp;
+    }
+};
